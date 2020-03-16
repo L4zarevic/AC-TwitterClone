1ef0d09643d95040edd77d1b12f986e34b69be84
Nemanja Lazarević <lazarevic.nemanja96@gmail.com>
2020-03-16 20:18:35 +0100

    Add refreshing options for HomePage

--- app/src/main/java/com/example/ac_twitterclone/HomePage.java
+++ app/src/main/java/com/example/ac_twitterclone/HomePage.java
@@ -8,6 +8,7 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -23,11 +24,12 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomePage extends Fragment {
+public class HomePage extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     private ListView listView;
     private ArrayList<HashMap<String, String>> tweetList;
     private SimpleAdapter adapter;
+    private SwipeRefreshLayout mySwipeRefreshLayout;
 
 
     public HomePage() {
@@ -39,17 +41,21 @@ public class HomePage extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+        final View view = inflater.inflate(R.layout.fragment_home_page, container, false);
 
         listView = view.findViewById(R.id.listOfTweets);
+        mySwipeRefreshLayout = view.findViewById(R.id.swipeContainer);
+
 
         tweetList = new ArrayList<>();
         adapter = new SimpleAdapter(getContext(), tweetList, android.R.layout.simple_list_item_2, new String[]{"tweetUserName", "tweetValue"}, new int[]{android.R.id.text1, android.R.id.text2});
 
+        mySwipeRefreshLayout.setOnRefreshListener(this);
         try {
 
             ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("MyTweet");
             parseQuery.whereContainedIn("user", ParseUser.getCurrentUser().getList("fanOf"));
+            parseQuery.whereNotContainedIn("tweet", tweetList);
             parseQuery.findInBackground(new FindCallback<ParseObject>() {
                 @Override
                 public void done(List<ParseObject> objects, ParseException e) {
@@ -69,8 +75,47 @@ public class HomePage extends Fragment {
             e.printStackTrace();
         }
 
+
         return view;
     }
 
+    //Manual refreshing home pages
+    @Override
+    public void onRefresh() {
 
+        tweetList = new ArrayList<>();
+        adapter = new SimpleAdapter(getContext(), tweetList, android.R.layout.simple_list_item_2, new String[]{"tweetUserName", "tweetValue"}, new int[]{android.R.id.text1, android.R.id.text2});
+
+        try {
+            ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("MyTweet");
+            parseQuery.whereContainedIn("user", ParseUser.getCurrentUser().getList("fanOf")).whereNotContainedIn("tweet", tweetList);
+            parseQuery.findInBackground(new FindCallback<ParseObject>() {
+                @Override
+                public void done(List<ParseObject> objects, ParseException e) {
+                    if (objects.size() > 0 && e == null) {
+                        for (ParseObject tweetObject : objects) {
+                            HashMap<String, String> userTweet = new HashMap<>();
+                            userTweet.put("tweetUserName", tweetObject.getString("user"));
+                            userTweet.put("tweetValue", tweetObject.getString("tweet"));
+                            tweetList.add(userTweet);
+                        }
+                        listView.setAdapter(adapter);
+                        if (mySwipeRefreshLayout.isRefreshing()) {
+                            mySwipeRefreshLayout.setRefreshing(false);
+                        }
+                    } else {
+                        if (mySwipeRefreshLayout.isRefreshing()) {
+                            mySwipeRefreshLayout.setRefreshing(false);
+                        }
+                    }
+                }
+            });
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
--- app/src/main/java/com/example/ac_twitterclone/LoginActivity.java
+++ app/src/main/java/com/example/ac_twitterclone/LoginActivity.java
@@ -99,5 +99,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         Intent intent = new Intent(LoginActivity.this, Twitter.class);
         startActivity(intent);
         finish();
+
     }
 }
